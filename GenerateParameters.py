ddd6e614fa452ff565e9a2c8c4f569220905965e
elu2 <60558250+elu@users.noreply.github.com>
2021-07-01 20:15:07 -0700

    Changed parameter generation method.

--- GenerateParameters.py
+++ GenerateParameters.py
@@ -4,6 +4,8 @@
 from random import uniform
 from math import log, exp
 import pandas as pd
+import time
+import csv
 
 params = {
     "K_1": [],
@@ -44,18 +46,27 @@ n_class = ["n_1", "n_2", "n_3", "n_4", "n_5", "n_6", "n_7", "n_8", "n_9", "n_10"
 tau_class = ["tau_MD", "tau_RP", "tau_EE"]
 beta_class = ["beta_MD", "beta_RP", "beta_EE"]
 
-n_sim = 20000
+# Initialize parameters
+pd.DataFrame(params).to_csv("parameters.csv", index=False)
+
+n_sim = 500000
+start = time.time()
 for i in range(n_sim):
+    param_vals = []
     for param in K_class:
-        params[param].append(round(exp(uniform(log(K_range[0]), log(K_range[1]))), 4))
+        param_vals.append(round(exp(uniform(log(K_range[0]), log(K_range[1]))), 4))
 
     for param in n_class:
-        params[param].append(round(exp(uniform(log(n_range[0]), log(n_range[1]))), 4))
+        param_vals.append(round(exp(uniform(log(n_range[0]), log(n_range[1]))), 4))
 
     for param in tau_class:
-        params[param].append(round(exp(uniform(log(tau_range[0]), log(tau_range[1]))), 4))
+        param_vals.append(round(exp(uniform(log(tau_range[0]), log(tau_range[1]))), 4))
 
     for param in beta_class:
-        params[param].append(round(exp(uniform(log(beta_range[0]), log(beta_range[1]))), 4))
+        param_vals.append(round(exp(uniform(log(beta_range[0]), log(beta_range[1]))), 4))
+        
+    with open("parameters.csv", 'a+', newline='') as file:
+        csv_writer = csv.writer(file)
+        csv_writer.writerow(param_vals)
 
-pd.DataFrame(params).to_csv("parameters.csv", index=False)
+print(f"{(time.time() - start)/60}min to load {n_sim} parameters.")
