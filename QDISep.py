1e4e0594d35525fecaad4c28234dd1c1fa4a894c
elu2 <60558250+elu2@users.noreply.github.com>
2021-08-06 17:20:23 -0700

    Create QDISep.py

---
+++ QDISep.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import os
+
+colnames = ['k_E', 'k_M', 'k_CD', 'k_CDS', 'k_R', 'k_RE', 'k_b', 'k_CE', 'k_I', 'k_P1', 'k_P2', 'k_DP', 'd_M', 'd_E', 'd_CD', 'd_CE', 'd_R', 'd_RP', 'd_RE', 'd_I', 'K_S', 'K_M', 'K_E', 'K_CD', 'K_CE', 'K_RP', 'K_P1', 'K_P2', "an_type", "dOnOff", "off_th", "on_th"]
+
+init_df = pd.DataFrame(columns=colnames)
+
+
+def checker(file_name):
+    if os.path.exists(file_name):
+        option = input("File exists. Overwrite? [y/n]").lower()
+        if option == "y":
+            init_df.to_csv({fle_name, index=False)
+            print("New empty file created.")
+        else:
+            print("Previous file retained.")
+
+    else:
+        init_df.to_csv(file_name, index=False)
+    
+    return
+
+
+checker("depthRun.csv")
