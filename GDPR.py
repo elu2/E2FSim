928096fd933d8ebdfd7e393ee1a84bd776955360
elu2 <60558250+elu2@users.noreply.github.com>
2021-08-13 15:16:17 -0700

    Bug fix.

--- GDPR.py
+++ GDPR.py
@@ -9,38 +9,6 @@ reps = 100
 lower_oom = 0.1
 upper_oom = 10
 
-params = {
-    "k_E": [],
-    "k_M": [],
-    "k_CD": [],
-    "k_CDS": [],
-    "k_R": [],
-    "k_RE": [],
-    "k_b": [],
-    "k_CE": [],
-    "k_I": [],
-    "k_P1": [],
-    "k_P2": [],
-    "k_DP": [],
-    "d_M": [],
-    "d_E": [],
-    "d_CD": [],
-    "d_CE": [],
-    "d_R": [],
-    "d_RP": [],
-    "d_RE": [],
-    "d_I": [],
-    "K_S": [],
-    "K_M": [],
-    "K_E": [],
-    "K_CD": [],
-    "K_CE": [],
-    "K_RP": [],
-    "K_P1": [],
-    "K_P2": [],
-    "an_type": []
-}
-
 scalars = np.logspace(np.log10(lower_oom), np.log10(upper_oom), num=reps)
 
 bistable_sets = pd.read_csv("bistableResults2017.csv")
@@ -49,6 +17,38 @@ if not os.path.exists("./depthParams/"):
     os.makedirs("./depthParams/")
 
 for i in range(bistable_sets.shape[0]):
+    params = {
+        "k_E": [],
+        "k_M": [],
+        "k_CD": [],
+        "k_CDS": [],
+        "k_R": [],
+        "k_RE": [],
+        "k_b": [],
+        "k_CE": [],
+        "k_I": [],
+        "k_P1": [],
+        "k_P2": [],
+        "k_DP": [],
+        "d_M": [],
+        "d_E": [],
+        "d_CD": [],
+        "d_CE": [],
+        "d_R": [],
+        "d_RP": [],
+        "d_RE": [],
+        "d_I": [],
+        "K_S": [],
+        "K_M": [],
+        "K_E": [],
+        "K_CD": [],
+        "K_CE": [],
+        "K_RP": [],
+        "K_P1": [],
+        "K_P2": [],
+        "an_type": []
+    }
+    
     base = bistable_sets.iloc[i, :-2].to_dict()
 
     for param_key in list(params.keys())[:-1]:
