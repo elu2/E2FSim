619aa6db81802103c59c49cd51ba6a41c367c46d
elu2 <60558250+elu2@users.noreply.github.com>
2021-08-13 11:10:04 -0700

    Default initializes 500 files.

--- QDIR.py
+++ QDIR.py
@@ -4,7 +4,7 @@
 import pandas as pd
 import os
 
-n = 100
+n = 500
 
 colnames = ['k_E', 'k_M', 'k_CD', 'k_CDS', 'k_R', 'k_RE', 'k_b', 'k_CE', 'k_I', 'k_P1', 'k_P2', 'k_DP', 'd_M', 'd_E', 'd_CD', 'd_CE', 'd_R', 'd_RP', 'd_RE', 'd_I', 'K_S', 'K_M', 'K_E', 'K_CD', 'K_CE', 'K_RP', 'K_P1', 'K_P2', "an_type", "dOnOff", "off_th", "on_th"]
 
