6821406f5c6dc43f9a8cc1cb4b608f46ae43fa78
elu2 <60558250+elu2@users.noreply.github.com>
2022-05-21 17:48:44 -0700

    Col names correspond with SDPR update

--- QDIR.py
+++ QDIR.py
@@ -6,7 +6,7 @@ import os
 
 n = 500
 
-colnames = ['k_E', 'k_M', 'k_CD', 'k_CDS', 'k_R', 'k_RE', 'k_b', 'k_CE', 'k_I', 'k_P1', 'k_P2', 'k_DP', 'd_M', 'd_E', 'd_CD', 'd_CE', 'd_R', 'd_RP', 'd_RE', 'd_I', 'K_S', 'K_M', 'K_E', 'K_CD', 'K_CE', 'K_RP', 'K_P1', 'K_P2', "an_type", "dOnOff", "off_th", "on_th"]
+colnames = ['k_E', 'k_M', 'k_CD', 'k_CDS', 'k_R', 'k_RE', 'k_b', 'k_CE', 'k_I', 'k_P1', 'k_P2', 'k_DP', 'd_M', 'd_E', 'd_CD', 'd_CE', 'd_R', 'd_RP', 'd_RE', 'd_I', 'K_S', 'K_M', 'K_E', 'K_CD', 'K_CE', 'K_RP', 'K_P1', 'K_P2', "an_type", "switch", "bistable", "resettable", "on_thresh", "off_thresh", "d_thresh"]
 
 if not os.path.exists("./depthRuns/"):
     os.makedirs("./depthRuns/")
