0b7b566cc05b2d5e2b6a655944849f97728c399c
elu2 <60558250+elu2@users.noreply.github.com>
2022-05-24 00:25:34 -0700

    Retaining off_thresh for downstream analyses.

--- BistableSeed.py
+++ BistableSeed.py
@@ -10,7 +10,7 @@ psr = pd.read_csv("./pre_seed_results.csv")
 psr = psr[(psr["bistable"] == True) & (psr["switch"] == True) & (psr["resettable"] == True)].reset_index(drop=True)
 trunc = min(psr.shape[0], trunc_upper)
 
-psr = psr.drop(["switch", "bistable", "resettable", "on_thresh", "off_thresh", "d_thresh"], axis=1)
+psr = psr.drop(["switch", "bistable", "resettable", "on_thresh", "d_thresh"], axis=1)
 psr = psr[:trunc]
 
 psr.to_csv("seed_sets.csv", index=False)
