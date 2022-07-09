1c0caa8e094d3594d196d5001956060b68176328
elu2 <60558250+elu2@users.noreply.github.com>
2022-07-09 15:13:44 -0700

    Update DRAnalysis.py

--- DRAnalysis.py
+++ DRAnalysis.py
@@ -1,28 +1,25 @@
-# DR Analaysis: get fold changes and perturbation point of two-fold change for each seeding parameter set
-# Required files: seed_set.csv, DRXXX.csv from depthRuns
-
 import pandas as pd
 import numpy as np
 import os
-from tqdm import tqdm
+from joblib import Parallel, delayed
 
 
 def get_subset(dr_df, param):
     scalars = np.logspace(np.log10(0.1), np.log10(10), num=100)
     scalars = pd.DataFrame(pd.Series(scalars)).rename(columns={0:"pert"})
-    
+
     # Add another col in next iteration: stable
     data_cols = ["switch", "bistable", "resettable", "on_thresh", "off_thresh", "d_thresh", "off_SS"]
     dr_df = dr_df[dr_df.an_type == param]
     dr_df = dr_df.sort_values(param)[[param] + data_cols].reset_index(drop=True)
-    
+
     assert dr_df.shape[0] == 100, f"Only {dr_df.shape[0]} simulations for {param}"
-    
+
     dr_df = pd.concat([scalars, dr_df], axis=1)
-    
+
     lower_df = dr_df.iloc[:50]
     upper_df = dr_df.iloc[50:]
-    
+
     return lower_df, upper_df
 
 
@@ -33,11 +30,11 @@ def get_fc(df, direction, seed_th):
     if direction == "lower":
         num_qual = met_crit.shape[0]
         fc = met_crit.off_thresh.iloc[0]/seed_th
-        
+
     if direction == "upper":
         num_qual = met_crit.shape[0]
         fc = met_crit.off_thresh.iloc[-1]/seed_th
-    
+
     return fc, num_qual
 
 
@@ -92,7 +89,7 @@ def dr_analysis(dr_num, seed_set_ths, path="./depthRuns/"):
     dr_df = pd.read_csv(f"{path}DR{dr_num}.csv")
 
     # get seeding set's threshold
-    seed_th = seed_set_ths[dr_num]
+    seed_th = seed_set_ths[0]
 
     lower_df, upper_df = get_subset(dr_df, "k_E")
     get_fc(lower_df, "lower", seed_th)
@@ -107,7 +104,7 @@ def dr_analysis(dr_num, seed_set_ths, path="./depthRuns/"):
 
         lower_pt = get_2f_change(lower_df, "lower", seed_th)
         upper_pt = get_2f_change(upper_df, "upper", seed_th)
-        
+
         nqual_dict[param + "_dec"] = lower_nval
         nqual_dict[param + "_inc"] = upper_nval
 
@@ -116,43 +113,21 @@ def dr_analysis(dr_num, seed_set_ths, path="./depthRuns/"):
 
         pt_dict[param + "_dec"] = lower_pt
         pt_dict[param + "_inc"] = upper_pt
-    
+
     # Fold change from seed set threshold
-    fc = pd.Series(fc_dict)
+    fc = pd.DataFrame(pd.Series(fc_dict)).transpose()
     # Point of reaching the two-fold significant value of seed set threshold
-    pt = pd.Series(pt_dict)
+    pt = pd.DataFrame(pd.Series(pt_dict)).transpose()
     # Number of qualified. If 50, it likely didn't end bistability within the ranges of the perturbation
-    nq = pd.Series(nqual_dict)
-    
+    nq = pd.DataFrame(pd.Series(nqual_dict)).transpose()
+
+    fc.to_csv("fc.csv", mode="a", index=False, header=False)
+    pt.to_csv("pt.csv", mode="a", index=False, header=False)
+    nq.to_csv("nq.csv", mode="a", index=False, header=False)
+
     return fc, pt, nq
 
 
-seed_set_ths = tuple(pd.read_csv("./seed_sets.csv")["off_thresh"])
-
-fc_df = []
-pt_df = []
-nq_df = []
-for i in tqdm(range(1000)):
-    fc, pt, nq = dr_analysis(i, seed_set_ths, path="./depthRuns/")
-    fc_df.append(fc)
-    pt_df.append(pt)
-    nq_df.append(nq)
-
-fc_df = pd.DataFrame.from_records(fc_df)
-pt_df = pd.DataFrame.from_records(pt_df)
-nq_df = pd.DataFrame.from_records(nq_df)
-
-mean_fc = np.mean(fc_df, axis=0)
-mean_fc = pd.DataFrame(mean_fc).reset_index()
-mean_fc.columns = ["param", "mean_fc"]
-mean_fc.sort_values("mean_fc").to_csv("./mean_fc.csv", index=False)
-
-mean_pt = np.mean(pt_df, axis=0)
-mean_pt = pd.DataFrame(mean_pt).reset_index()
-mean_pt.columns = ["param", "mean_pt"]
-mean_pt.sort_values("mean_pt").to_csv("./mean_pt.csv", index=False)
-
-mean_nq = np.mean(nq_df, axis=0)
-mean_nq = pd.DataFrame(mean_nq).reset_index()
-mean_nq.columns = ["param", "mean_nq"]
-mean_nq.sort_values("mean_nq").to_csv("./mean_nq.csv", index=False)
+if __name__ == "__main__":
+    seed_set_ths = tuple(pd.read_csv("./seed_sets.csv")["off_thresh"])
+    Parallel(n_jobs=-1)(delayed(dr_analysis)(dr_num, seed_set_ths) for dr_num in range(len(os.listdir("./depthRuns/")))
